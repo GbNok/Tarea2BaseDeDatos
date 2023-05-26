264bde516c407647ee3653a51a8fc92024df26e0
GbNok <gnokwsie@gmail.com>
2023-05-26 00:17:08 -0400

    Fix tables

--- Prestigetravels_db/Prestigetravels.sql
+++ Prestigetravels_db/Prestigetravels.sql
@@ -8,61 +8,59 @@ CREATE TABLE usuario (
 );
 
 CREATE TABLE carrito (
---   id_carrito INT AUTO_INCREMENT PRIMARY KEY,
-  id_usuario INT, 
-  FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario),
-  hotel_paquete_id INT,
-  nombre VARCHAR(100),
-  precio INT,
-  total_pagar INT,
-  total_pagar_descuento DECIMAL(10, 2),
-  FOREIGN KEY (hotel_paquete_id) REFERENCES hoteles(id_hotel) ON DELETE CASCADE,
-  FOREIGN KEY (hotel_paquete_id) REFERENCES paquetes(id_paquete) ON DELETE CASCADE
-);
-
-CREATE TABLE hoteles (
-  id_hotel INT AUTO_INCREMENT PRIMARY KEY,
-  nombre VARCHAR(100),
---   numero_estrellas INT,
-  precio_noche INT,
-  ciudad VARCHAR(50),
-  habitaciones_totales INT,
-  habitaciones_disponibles INT,
-  estacionamiento BOOLEAN,
-  piscina BOOLEAN,
-  lavanderia BOOLEAN,
-  pet_friendly BOOLEAN,
-  servicio_desayuno BOOLEAN
-);
-
-CREATE TABLE paquetes (
-  id_paquete INT AUTO_INCREMENT PRIMARY KEY,
-  nombre VARCHAR(100),
-  aerolinea_ida VARCHAR(50),
-  hospedaje1 INT REFERENCES hoteles(id_hotel),
-  hospedaje2 INT REFERENCES hoteles(id_hotel),
-  hospedaje3 INT REFERENCES hoteles(id_hotel),
-  ciudad1 VARCHAR(50),
-  ciudad2 VARCHAR(50),
-  ciudad3 VARCHAR(50),
-  aerolinea_vuelta VARCHAR(50),
-  fecha_salida DATE,
-  fecha_llegada DATE,
-  noches_totales INT,
-  precio_persona INT,
-  paquetes_disponibles INT,
-  paquetes_totales INT,
-  max_personas_paquete INT
+    id_usuario INT,
+    id_paquete INT,
+    FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario),
+    FOREIGN KEY (id_paquete) REFERENCES paquete(id_paquete)
+    precio INT,
+    total_pagar INT,
+    total_pagar_descuento DECIMAL(10, 2),
 );
 
 
 CREATE TABLE wishlist (
-  usuario_id INT,
-  hotel_paquete_id INT,
-  FOREIGN KEY (usuario_id) REFERENCES usuario(id_usuario),
-  FOREIGN KEY (hotel_paquete_id) REFERENCES hoteles(id_hotel) ON DELETE CASCADE,
-  FOREIGN KEY (hotel_paquete_id) REFERENCES paquetes(id_paquete) ON DELETE CASCADE,
-  PRIMARY KEY (usuario_id, hotel_paquete_id)
+    id_usuario INT,
+    id_paquete INT,
+    FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario),
+    FOREIGN KEY (id_paquete) REFERENCES paquete(id_paquete)
+);
+
+CREATE TABLE paquete (
+    id_paquete INT AUTO_INCREMENT PRIMARY KEY,
+);
+
+CREATE TABLE reserva (
+    id_paquete INT,
+    id_hotel INT,
+    FOREIGN KEY (id_paquete) REFERENCES paquete(id_paquete),
+    FOREIGN KEY (id_hotel) REFERENCES hotel(id_hotel),
+    fecha_inicio DATE,
+    fecha_fin DATE
+);
+
+CREATE TABLE vuelos (
+    id_paquete INT,
+    FOREIGN KEY (id_paquete) REFERENCES paquete(id_paquete),
+    aerolinea VARCHAR(50),
+    aeropuerto_salida VARCHAR(10), 
+    aeropuerto_llegada VARCHAR(10),
+    fecha_salida DATE,
+    fecha_llegada DATE 
+);
+
+
+CREATE TABLE hotel (
+    id_hotel INT AUTO_INCREMENT PRIMARY KEY,
+      nombre VARCHAR(100),
+    precio_noche INT,
+    ciudad VARCHAR(50),
+    habitaciones_totales INT,
+    habitaciones_disponibles INT,
+    estacionamiento BOOLEAN,
+    piscina BOOLEAN,
+    lavanderia BOOLEAN,
+    pet_friendly BOOLEAN,
+    servicio_desayuno BOOLEAN
 );
 
 CREATE TABLE rating (
