ab791d6484cdd6b4058c107d4e3df77a52e66635
zamquin <zamquin@gmail.com>
2023-06-02 17:26:07 -0400

    select list hotel rating

--- Prestigetravels/views/rating_view.php
+++ Prestigetravels/views/rating_view.php
@@ -1,12 +1,23 @@
 <?php 
-// foreach($hotels as &$hotel) {
-//     echo $hotel["nombre"];
-// }
-// ?>
+require_once "../db.php";
+?>
 
 <form acction="/rating.php" method="POST" class="box">
     <input class="input" type="text" placeholder="comentario (opcional)" name="comment">
     <input class="input" type="number" placeholder="rating" name="rating">
-    <input class="input" type="number" placeholder="hotel" name="hotel_id">
+    <select name="hotel_id" style="width: 150px; height: 20px;">
+  <?php
+
+  $db = DB::getInstance();
+
+  $sql = "SELECT id_hotel, nombre FROM hotel";
+  $stmt = $db->query($sql);
+
+  while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    echo '<option value="' . $row["id_hotel"] . '">' . $row["nombre"] . '</option>';
+  }
+  ?>
+</select>
+
     <button> Publicar </button>
 </form>
