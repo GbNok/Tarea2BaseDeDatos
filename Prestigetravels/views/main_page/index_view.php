ee49b9817585605730844ff99dc87c2461ec03de
GbNok <gnokwsie@gmail.com>
2023-06-03 15:29:39 -0400

    Fix index view

--- Prestigetravels/views/main_page/index_view.php
+++ Prestigetravels/views/main_page/index_view.php
@@ -5,22 +5,22 @@
 
 <section class="section px-0">
   <div class="columns">
-    <? foreach ($highest_availability as &$product): ?>
+    <?php foreach ($highest_availability as &$product){ ?>
     <div class="column is-3">
       <?php View::fragment('views/main_page/product_card.php', ['product' => $product]) ?>
     </div>
-    <? endforeach ?>
+    <?php } ?>
   </div>
 </section>
 
 <h3 class="title is-3">Mejores Raitings</h3>
 
 <section class="section px-0">
-  <? foreach ($highest_rated as $i => &$product): ?>
+  <?php foreach ($highest_rated as $i => &$product){ ?>
   <?= $i % 4 === 0 && $i != 0 ? '</div>' : '' ?>
   <?= $i % 4 === 0 ? '<div class="columns">' : '' ?>
   <div class="column is-3">
     <?php View::fragment('views/main_page/product_card.php', ['product' => $product]) ?>
   </div>
-  <? endforeach ?>
+  <?php } ?>
 </section>
