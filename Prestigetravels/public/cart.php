2481c244eb501f93d4d4596c3d6a96c34d171089
zamquin <zamquin@gmail.com>
2023-06-04 21:53:30 -0400

    10% descuento aleatoriamente done
    manda la info a cart_view con items, precio total(con el descuento) y el descuento y el precio sin descuento

--- Prestigetravels/public/cart.php
+++ Prestigetravels/public/cart.php
@@ -13,14 +13,23 @@ $method = $_SERVER["REQUEST_METHOD"];
 if ($method === "GET"){
     $user_id = $_SESSION["user"]["id"];
     $items = Cart::getAll($user_id);
-    
+    $precio_total = Cart::getTotalCartPrice($user_id); 
+    $descuento = 0;
+    if (isset($_SESSION['offer_accepted'])) {
+        $precio_total *= 0.9;
+        $descuento = 10;
+    }
+
     View::render('cart_view.php', [
         'user_id' => $user_id,
-        'items' => $items
+        'items' => $items,
+        'precio_total_no_discount' => Cart::getTotalCartPrice($user_id),
+        'precio_total' => $precio_total,
+        'descuento' => $descuento
     ]);
 
 
-    
+
 } elseif ($method === "POST"){
     // borrar
 }
